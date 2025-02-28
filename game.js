cf08f72c5df8167268154e87e9edaf32489d62de
0xmopersie <136506983+0xmopersie@users.noreply.github.com>
2025-02-28 12:25:47 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-// ZK Runner - Fully Enhanced Version
-// Improved visuals, mechanics, and player experience
+// ZK Runner - Chrome Dinosaur Style Game
+// Endless runner game similar to Google Chrome offline dinosaur game
 
 const config = {
     type: Phaser.AUTO,
@@ -8,7 +8,7 @@ const config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 500 },
+            gravity: { y: 600 },
             debug: false
         }
     },
@@ -22,7 +22,6 @@ const config = {
 let player;
 let cursors;
 let obstacles;
-let zkProofs;
 let score = 0;
 let scoreText;
 let gameOverText;
@@ -32,39 +31,28 @@ const game = new Phaser.Game(config);
 
 function preload() {
     this.load.image('player', 'https://cdn-icons-png.flaticon.com/512/4144/4144600.png');
-    this.load.image('ground', 'https://cdn-icons-png.flaticon.com/512/646/646094.png');
+    this.load.image('ground', 'https://cdn-icons-png.flaticon.com/512/190/190008.png');
     this.load.image('obstacle', 'https://cdn-icons-png.flaticon.com/512/564/564619.png');
-    this.load.image('zkProof', 'https://cdn-icons-png.flaticon.com/512/845/845646.png');
     this.load.image('restart', 'https://cdn-icons-png.flaticon.com/512/3031/3031293.png');
     this.load.audio('jump', 'https://assets.mixkit.co/active_storage/sfx/1555/1555-preview.mp3');
-    this.load.audio('collect', 'https://assets.mixkit.co/active_storage/sfx/1805/1805-preview.mp3');
 }
 
 function create() {
     this.add.rectangle(400, 200, 800, 400, 0x000000);
+    
     let ground = this.physics.add.staticGroup();
-    ground.create(400, 390, 'ground').setScale(1, 0.5).refreshBody();
+    ground.create(400, 390, 'ground').setScale(4, 0.5).refreshBody();
     
     player = this.physics.add.sprite(100, 300, 'player');
     player.setCollideWorldBounds(true);
-    player.setPosition(100, 300);
-    player.setScale(0.3);
-    player.setVelocityX(100); 
+    player.setScale(0.15);
     this.physics.add.collider(player, ground);
     
     obstacles = this.physics.add.group();
-    zkProofs = this.physics.add.group();
     
     this.time.addEvent({
-        delay: 1800,
+        delay: 1500,
         callback: spawnObstacle,
-        callbackScope: this,
-        loop: true
-    });
-    
-    this.time.addEvent({
-        delay: 2500,
-        callback: spawnZKProof,
         callbackScope: this,
         loop: true
     });
@@ -73,7 +61,6 @@ function create() {
     scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '24px', fill: '#fff' });
     
     this.physics.add.collider(player, obstacles, gameOver, null, this);
-    this.physics.add.overlap(player, zkProofs, collectZKProof, null, this);
     
     gameOverText = this.add.text(400, 150, '', { fontSize: '32px', fill: '#ff0000' }).setOrigin(0.5);
     restartButton = this.add.image(400, 250, 'restart').setScale(0.1).setInteractive().setVisible(false);
@@ -90,20 +77,7 @@ function update() {
 function spawnObstacle() {
     let obstacle = obstacles.create(800, 360, 'obstacle');
     obstacle.setScale(0.1);
-    obstacle.setVelocityX(-220);
-}
-
-function spawnZKProof() {
-    let zkProof = zkProofs.create(800, Phaser.Math.Between(250, 350), 'zkProof');
-    zkProof.setScale(0.1);
-    zkProof.setVelocityX(-220);
-}
-
-function collectZKProof(player, zkProof) {
-    zkProof.destroy();
-    score += 10;
-    scoreText.setText('Score: ' + score);
-    this.sound.play('collect');
+    obstacle.setVelocityX(-300);
 }
 
 function gameOver() {
